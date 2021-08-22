1157444959c4e6904db6dfdbcbc400e58d637ad6
Evandro Ladislau <evandroladislau23@gmail.com>
2021-08-22 00:18:19 -0300

    Finalizando segregação de modulos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,9 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule } from '@angular/router';
 import { HttpClientModule } from '@angular/common/http';
-
 import { AppComponent } from './app.component';
-import { Error404Component } from './error-404/error-404.component';
 import { CourseInfoComponent } from './courses/course-info.component';
 import { CoreModule } from './core/core.module';
 import { CurseModule } from './courses/course.module';
@@ -12,7 +10,6 @@ import { CurseModule } from './courses/course.module';
 @NgModule({
   declarations: [
     AppComponent,
-    Error404Component,
     CourseInfoComponent
 
   ],
@@ -27,10 +24,6 @@ import { CurseModule } from './courses/course.module';
     RouterModule.forRoot([
       {
         path: '',redirectTo: 'courses',pathMatch:'full'
-      },
-
-      {
-        path: '**', component: Error404Component
       }
     ])
 
